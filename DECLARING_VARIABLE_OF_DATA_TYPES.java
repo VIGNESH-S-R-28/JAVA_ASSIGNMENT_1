6dccf7342aa84b06b4b7627605bc305f210f1168
VIGNESH RAJENDRAN <89457100+VIGNESH-S-R-28@users.noreply.github.com>
2021-08-28 13:21:59 +0530

    DECLARING_VARIABLE_OF_DATA_TYPES.java

---
+++ DECLARING_VARIABLE_OF_DATA_TYPES.java
@@ -0,0 +1,14 @@
+
+public class DECLARING_VARIABLE_OF_DATA_TYPES {
+
+	public static void main(String[] args) {
+		
+		int number= 8;
+		float floatnumber = 8.99f;
+		char character = 'A';
+		boolean booleanval = true;
+		String name = "Vignesh";
+		
+	}
+
+}
